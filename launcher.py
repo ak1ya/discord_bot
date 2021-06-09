9d4f5d406bf0202146bd2cb72f9f4e750b99e5d7
ak1ya <sasanoha.payapaya@gmail.com>
2021-06-09 16:45:05 +0900

    first commit

---
+++ launcher.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+import config
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print("on_ready")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        # Bot からのメッセージには反応しない
+        # この判定をしないと無限ループが起きる
+        return
+
+    if "Bot" in message.content:
+        await message.channel.send(" Bot ")
+
+bot.run(config.TOKEN)
